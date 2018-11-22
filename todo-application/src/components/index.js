377adf52ffee68b94e8bd2748d2bf2bf943a5de6
godmode97 <neiljames97@gmail.com>
2018-11-22 18:17:11 +0800

    modified component

--- todo-application/src/components/index.js
+++ todo-application/src/components/index.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
-import logo from '../resources/images/logo.svg';
+import ToDoListItems from './layouts/todo_list_items';
 import '../resources/css/styles.css';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      value: '',
+      defaultValue: ''
+    }
+
+    this.onHandleTextChange = this.onHandleTextChange.bind(this);
+    this.onHandleSubmit = this.onHandleSubmit.bind(this);
+  }
+
+  onHandleTextChange(event){
+    this.setState({ value: event.target.value })
+  }
+
+  onHandleSubmit(event){
+    event.preventDefault();
+    this.props.toDoAdded(this.state.value);
+    this.setState({ value: "" })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <form onSubmit={ this.onHandleSubmit }>
+          <input type="text"
+                 placeholder="Add To Do..."
+                 onChange={ this.onHandleTextChange }
+                 value={this.state.value}
+          />
+        </form>
+        <ToDoListItems datasource={this.props.state.todoItems} />
       </div>
     );
   }
