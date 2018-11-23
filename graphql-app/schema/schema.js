db3f4e3cfd944f252db6f84b8c5d98418790ff9b
godmode97 <neiljames97@gmail.com>
2018-11-23 10:54:48 +0800

    created schema

---
+++ graphql-app/schema/schema.js
@@ -0,0 +1,33 @@
+const graphql = require('graphql');
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+const _ = require('lodash');
+const axios = require('axios');
+
+
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLInt }
+  }
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args){
+        console.log(parentValue);
+        return axios.get(`http://localhost:3000/users/${args.id}`)
+          .then(({data}) => data)
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+})
